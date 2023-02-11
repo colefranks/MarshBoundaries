54f3c02e352c92f1febafec7f176ea0b04b00c2d
colefranks <maester.cole@gmail.com>
2023-02-10 17:04:42 -0800

    all

--- PredictFromSaved.py
+++ PredictFromSaved.py
@@ -9,12 +9,7 @@ import matplotlib.pyplot as plt
 XDIM = 2048
 YDIM = 11*XDIM//8
 
-#XDIM = 512
-#YDIM = 11*XDIM//8
-
-
-#for now 
-#FOLDER = "train_jpg/VCR"
+LINE = 3*XDIM//8
 
 #assumes the images in the folder look like Band1.jpg, Band2.jpg etc
 def folder_to_array(folder):
@@ -28,7 +23,6 @@ def folder_to_array(folder):
             img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
             img = cv.resize(img,(YDIM, XDIM))
             images.append(img)
-    images = np.array([images])
     return images
 
 #convert image to tensor that can go into model
@@ -53,14 +47,17 @@ CLASS_DICT = {0: 'marsh', 1: 'water', 2: 'upland', 3: 'unlabeled'}
 
 def pred_from_saved(
     model_path = 'saved-models/Depth2RandomFlip89val',
-    folder_path = "train_jpg/PIE",
+    folder_path = "train_jpg",
     startx = 0, 
     starty = 0, 
     zoomx = 1000,
     zoomy=1000,
     fig_size=5
     ):
-
+    pie_path = f'{folder_path}/PIE_scaled'
+    vcr_path = f'{folder_path}/VCR_scaled'
+    gce_path = f'{folder_path}/GCE_scaled'
+    
     startx = min(XDIM,startx)
     starty = min(XDIM,startx)
     endx = min(XDIM,startx + zoomx)
@@ -69,29 +66,43 @@ def pred_from_saved(
     zoomx = min(XDIM,zoomx)
     zoomy = min(YDIM,zoomy)
 
-    WHICH_IMG = 0
     WHICH_BAND = 4
     BANDS = 11
 
     model = tf.keras.models.load_model(model_path)
 
-    images = folder_to_array(folder_path)
-
-    small_image = get_tensor_pred(images[WHICH_IMG,:,startx:endx ,starty:endy].reshape([1,BANDS,zoomx,zoomy]))
-
-    small_1hot = model.predict(small_image)
-
-    small_pred = create_mask_pred(small_1hot)
+    images = np.array([folder_to_array(gce_path), folder_to_array(pie_path), folder_to_array(vcr_path)])
+    masks = np.load('numpymasks.npy')
+    masks[:,:,LINE-5:LINE+5,:]=np.nan
+    small_masks = masks[:,0,startx:endx ,starty:endy]
 
     num_class = len(CLASS_DICT)
 
-
+    site_dict = {0:'GCE', 1:'PIE', 2:'VCR'}
+    site_perm = {0:1, 1:2, 2:0}
+    COLOR = 'red'
+    plt.rcParams['text.color'] = COLOR
+    plt.rcParams['axes.labelcolor'] = COLOR
+    plt.rcParams['xtick.color'] = COLOR
+    plt.rcParams['ytick.color'] = COLOR
     fig = plt.figure(figsize=(fig_size, fig_size))
-    ax1 = fig.add_subplot(121)
-    ax1.imshow(small_image[0,:,:,WHICH_BAND])
-    cmap = plt.cm.get_cmap('viridis', num_class)
-    ax2= fig.add_subplot(122)
-    im2 = ax2.imshow(small_pred,vmin=0, vmax=num_class-1,cmap=cmap)
+    for i in range(3):
+        small_image = get_tensor_pred(images[site_perm[i],:,startx:endx ,starty:endy].reshape([1,BANDS,zoomx,zoomy]))
+        small_1hot = model.predict(small_image)
+        small_pred = create_mask_pred(small_1hot)
+        ax1 = fig.add_subplot(int(str(f'33{i*3+1}')))
+        ax1.imshow(small_image[0,:,:,WHICH_BAND])
+        ax1.set_title("landsat image")
+        ax1.set_ylabel(site_dict[site_perm[i]])
+        cmap = plt.cm.get_cmap('viridis', num_class)
+        ax2= fig.add_subplot(int(str(f'33{i*3+2}')))
+        ax2.set_title("predicted classification")
+        im2 = ax2.imshow(small_pred,vmin=0, vmax=num_class-1,cmap=cmap)
+        cmap = plt.cm.get_cmap('viridis', num_class)
+        ax3 = fig.add_subplot(int(str(f'33{i*3+3}')))
+        ax3.set_title("actual classification")
+        im2 = ax3.imshow(small_masks[site_perm[i]],vmin=0, vmax=num_class-1,cmap=cmap)
+        cmap = plt.cm.get_cmap('viridis', num_class)
     cbar = plt.colorbar(
         im2, 
         ticks=[3/8 + i*((num_class-1.0)/num_class) for i in range(num_class)],
